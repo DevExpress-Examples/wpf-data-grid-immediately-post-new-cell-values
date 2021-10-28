0a0218ba9f571802804b24bb02a1d0966483caac
aliev.faramoz <faramoz.aliev@devexpress.com>
2021-10-28 18:38:56 +0300

    update

--- CS/UpdateOnPropertyChanged/App.xaml.cs
+++ CS/UpdateOnPropertyChanged/App.xaml.cs
@@ -1,41 +1,5 @@
-﻿// Developer Express Code Central Example:
-// How to make GridControl immediately save changes in a cell after editing
-// 
-// This example shows how to update GridControl's data source right after a cell
-// editor value has been changed. To implement this scenario, we created a custom
-// helper class exposing the CommitHelper.CommitOnValueChanged attached
-// property.
-// 
-// This example uses the same idea as the one demonstrated in the
-// http://www.devexpress.com/scid=E1801 thread. If you don't want to use custom
-// helpers, check the solution from the E1801 example instead.
-// 
-// You can find sample updates and versions for different programming languages here:
-// http://www.devexpress.com/example=E4155
+﻿using System.Windows;
 
-// Developer Express Code Central Example:
-// How to make GridControl immediately save changes in a cell after editing
-// 
-// This example shows how to update GridControl DataSource right after a cell
-// editor value has been changed. To implement this scenario, we have introduced
-// the CommitHelper.CommitOnValueChanged attached property.
-// 
-// You can find sample updates and versions for different programming languages here:
-// http://www.devexpress.com/example=E4155
-
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Windows;
-
-namespace UpdateOnPropertyChanged
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+namespace UpdateOnPropertyChanged {
+    public partial class App : Application { }
 }
---
+++ CS/UpdateOnPropertyChanged/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using DevExpress.Mvvm;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace UpdateOnPropertyChanged {
+    public class Item : BindableBase {
+        public int ID {
+            get { return GetValue<int>(); }
+            set { SetValue(value); }
+        }
+        public string Name {
+            get { return GetValue<string>(); }
+            set { SetValue(value); }
+        }
+    }
+
+    public class MainViewModel : ViewModelBase {
+        public ObservableCollection<Item> Items {
+            get { return GetValue<ObservableCollection<Item>>(); }
+            set { SetValue(value); }
+        }
+        public Item SelectedItem {
+            get { return GetValue<Item>(); }
+            set { SetValue(value); }
+        }
+
+        public MainViewModel() {
+            Items = GetData();
+        }
+
+        static ObservableCollection<Item> GetData() {
+            var collection = Enumerable.Range(0, 5).Select(x => new Item { ID = x, Name = "Name" + x });
+            return new ObservableCollection<Item>(collection);
+        }
+    }
+}
